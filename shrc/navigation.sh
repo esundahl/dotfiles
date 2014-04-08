4cf34a1afec904219ee6425e6c16d89e0ba4ce66
Erik Sundahl <esundahl@gmail.com>
2014-04-08 09:07:24 -0700

    updates navigation aliases

--- shrc/navigation.sh
+++ shrc/navigation.sh
@@ -6,7 +6,6 @@ alias prj='cd ~/projects'
 alias bashp='vim ~/.bash_profile'
 alias vimrc='vim ~/.vimrc.after'
 alias vimcs='vim ~/Dropbox/settings/vim/cheat_sheet.md'
-alias notes='vim ~/Dropbox/notes/'
+alias notes='atom ~/Dropbox/notes/'
 alias ..='cd ..'
-source "$HOME/Dropbox/settings/shell/marks.sh"
 ##################################
