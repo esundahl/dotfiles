c6c211587de592ddfd04da13ec5ac2ab7c39ea99
Erik Sundahl <esundahl@gmail.com>
2015-04-08 14:07:04 -0700

    add docker-machine alias

--- shrc/docker.sh
+++ shrc/docker.sh
@@ -1,3 +1,10 @@
 export DOCKER_HOST=tcp://192.168.59.103:2376
 export DOCKER_CERT_PATH=/Users/esundahl/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
+
+##########################
+# Alias
+##########################
+alias dc="docker-compose"
+alias dm="docker-machine"
+###########################
