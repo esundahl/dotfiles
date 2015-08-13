0406ca4fbdd35f2e42f1ecc3c46564a7b9bbdad7
Erik Sundahl <esundahl@gmail.com>
2015-08-13 11:41:08 -0700

    remove obsolete alias

--- shrc/go.sh
+++ shrc/go.sh
@@ -2,7 +2,7 @@
 # Go Aliases
 ###################################
 alias gr='go run'
-alias gb='go build'
 ##################################
 
 export GOPATH=$HOME/code/go/
+export PATH=$PATH:$GOPATH/bin
--- shrc/node.sh
+++ shrc/node.sh
@@ -1,6 +1,2 @@
 export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
-
-alias gulp='node_modules/.bin/gulp'
-alias supervisor='node_modules/supervisor/lib/cli-wrapper.js'
-alias nodemon='node_modules/.bin/nodemon'
