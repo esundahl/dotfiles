29764e4f39e4c15b2e31f5dacd261867d9d9a385
Erik Sundahl <esundahl@gmail.com>
2016-10-05 08:54:08 -0700

    remove docker-machine envs

--- shrc/docker.sh
+++ shrc/docker.sh
@@ -1,7 +1,3 @@
-export DOCKER_TLS_VERIFY=1
-export DOCKER_CERT_PATH="/Users/esundahl/.docker/machine/machines/polyverse"
-export DOCKER_HOST=tcp://192.168.99.100:2376
-
 ##########################
 # Alias
 ##########################
